4c80bf57b1cc324cc032ae3dd9e658e5631151af
Sonu <sonualam1599@gmail.com>
2021-10-08 08:32:43 +0530

    chores update

--- Q6/index.js
+++ Q6/index.js
@@ -6,22 +6,53 @@ const preFinalMarks = document.querySelector("#preFinal-marks");
 // console.log(preFinalMarks)
 const finalMarks = document.querySelector("#final-marks");
 const resultBtn = document.querySelector("#result-btn");
+const nextBtn = document.querySelector("#next-btn");
 
 
+let count = 0;
+let data = [];
+
+function clearTable() {
+    if (count < 5) {
+        addStudentData();
+        count++;
+    } else {
+        calculateResuslt();
+    }
+
+    // console.log("from cleart")
+
+}
 
 
 function calculateResuslt() {
+
     var totalMarks = Number(unitMarks.value) + Number(preFinalMarks.value) + Number(finalMarks.value)
     // console.log(totalMarks)
 
     var averageMarks = totalMarks / 4;
-
     //display the results
-    document.querySelector("#std-name").value = document.querySelector("#student-name").value;
-    document.querySelector("#total-marks").value = totalMarks;
-    document.querySelector("#average-marks").value = averageMarks;
+    // document.querySelector("#std-name").value = document.querySelector("#student-name").value;
+    // document.querySelector("#total-marks").value = totalMarks;
+    // document.querySelector("#average-marks").value = averageMarks;
+
+    var studentName = document.getElementById('student-name').value;
+    var totalMarks = document.getElementById('total-marks');
+    var averageMarks = document.getElementById('')
+
+
+
+
 }
 
+// function addStudentData() {
+
+//     display the results
+//     document.querySelector("#student-name") = '';
+//     document.querySelector("#unit-marks") = '';
+//     document.querySelector("#preFinal-marks") = '';
+//     document.querySelector("#final-marks") = '';
+// }
 
 
 
@@ -29,7 +60,9 @@ function calculateResuslt() {
 
 
 
-resultBtn.addEventListener("click", calculateResuslt)
+
+resultBtn.addEventListener("click", calculateResuslt);
+nextBtn.addEventListener("click", clearTable);
 
 
 
